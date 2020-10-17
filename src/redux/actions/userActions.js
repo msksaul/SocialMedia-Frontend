71a9618a02f6c2fd4920a50299ad441b0be3f9a7
msksaul <64514098+msksaul@users.noreply.github.com>
2020-10-17 17:43:43 -0600

    Signup and Auth Route

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -1,14 +1,11 @@
-import { SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI } from '../types'
+import { SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI, SET_UNAUTHENTICATED } from '../types'
 import axios from 'axios'
 
 export const loginUser = (userData, history) => (dispatch) => {
   dispatch({ type: LOADING_UI })
   axios.post('/login', userData)
       .then(res => {
-        console.log(res.data)
-        const FBIdToken = `Bearer ${res.data.token}`
-        localStorage.setItem('FBIdToken', FBIdToken)
-        axios.defaults.headers.common['Autorization'] = FBIdToken
+        setAuthorizationHeader(res.data.token)
         dispatch(getUserData())
         dispatch({ type: CLEAR_ERRORS })
         history.push('/')
@@ -21,6 +18,12 @@ export const loginUser = (userData, history) => (dispatch) => {
       })
 }
 
+export const logoutUser = () => (dispatch) => {
+  localStorage.removeItem('FBIdToken')
+  delete axios.defaults.headers.common['Authorization']
+  dispatch({ type: SET_UNAUTHENTICATED })
+}
+
 export const getUserData = () => (dispatch) => {
   axios.get('/user')
     .then(res => {
@@ -30,4 +33,27 @@ export const getUserData = () => (dispatch) => {
       })
     })
     .catch(err => console.log(err))
+}
+
+export const signupUser = (newUserData, history) => (dispatch) => {
+  dispatch({ type: LOADING_UI })
+  axios.post('/signup', newUserData)
+      .then(res => {
+        setAuthorizationHeader(res.data.token)
+        dispatch(getUserData())
+        dispatch({ type: CLEAR_ERRORS })
+        history.push('/')
+      })
+      .catch(err => {
+        dispatch({
+          type: SET_ERRORS,
+          payload: err.response.data
+        })
+      })
+}
+
+const setAuthorizationHeader = (token) => {
+  const FBIdToken = `Bearer ${token}`
+  localStorage.setItem('FBIdToken', FBIdToken)
+  axios.defaults.headers.common['Autorization'] = FBIdToken
 }
