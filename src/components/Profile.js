46bc4f1ed0ab8cd56dbd4a6113861b6d3dd0ffcb
msksaul <64514098+msksaul@users.noreply.github.com>
2020-10-19 16:54:18 -0600

    Image Upload

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -8,12 +8,16 @@ import Button from '@material-ui/core/Button'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
 import MuiLink from '@material-ui/core/Link'
+import IconButton from '@material-ui/core/IconButton'
+import EditIcon from '@material-ui/icons/Edit'
+import Tooltip from '@material-ui/core/Tooltip'
 
 import LocationOn from '@material-ui/icons/LocationOn'
 import LinkIcon from '@material-ui/icons/Link'
 import CalendarToday from '@material-ui/icons/CalendarToday'
 
 import { connect } from 'react-redux'
+import { logoutUser, uploadImage } from '../redux/actions/userActions'
 
 const styles = {
   paper: {
@@ -64,6 +68,19 @@ const styles = {
 }
 
 class Profile extends Component {
+
+  handleImageChange = (event) => {
+    const image = event.target.files[0]
+    const formData = new FormData()
+    formData.append('image', image, image.name)
+    this.props.uploadImage(formData)
+  }
+
+  handleEditPicture = () => {
+    const fileInput = document.getElementById('imageInput')
+    fileInput.click()
+  }
+
   render() {
 
     const { classes, user: { credentials: { handle, createdAt, imageUrl, bio, website, location }, loading, authenticated } } = this.props
@@ -73,6 +90,12 @@ class Profile extends Component {
         <div className={classes.profile}>
           <div className='image-wrapper'>
             <img src={imageUrl} alt='profile' className='profile-image'/>
+            <input hidden='hidden' type='file' id='imageInput' onChange={this.handleImageChange}/>
+            <Tooltip title='Edit profile picture' placement='top'>
+              <IconButton onClick={this.handleEditPicture} className='button'>
+                <EditIcon color='primary'/>
+              </IconButton>
+            </Tooltip>
           </div>
           <hr/>
           <div className='profile-details'>
@@ -91,7 +114,7 @@ class Profile extends Component {
             {website && (
               <Fragment>
                 <LinkIcon color='primary'/>
-                <a href={website} target='_blank' rel='noopener noreferer'>
+                <a href={website} target='_blank' rel='noopener noreferrer'>
                   {' '}{website}
                 </a>
                 <hr/>
@@ -126,9 +149,13 @@ const mapStateToProps = (state) => ({
   user: state.user
 })
 
+const mapActionsToProps = { logoutUser,  uploadImage }
+
 Profile.propTypes = {
+  logoutUser: PropTypes.func.isRequired,
+  uploadImage: PropTypes.func.isRequired,
   user: PropTypes.object.isRequired,
   classes: PropTypes.object.isRequired
 }
 
-export default connect(mapStateToProps)(withStyles(styles)(Profile))
+export default connect(mapStateToProps, mapActionsToProps)(withStyles(styles)(Profile))
--- src/util/AuthRoute.js
+++ src/util/AuthRoute.js
@@ -12,7 +12,7 @@ const mapStateToProps = (state) => ({
 })
 
 AuthRoute.propTypes = {
-  user: PropTypes.object.isRequired
+  user: PropTypes.object
 }
 
 export default connect(mapStateToProps)(AuthRoute)
