360dced512df546f0645f74347c599288804d778
msksaul <64514098+msksaul@users.noreply.github.com>
2021-01-05 15:37:57 -0600

    User Page

--- src/redux/actions/dataActions.js
+++ src/redux/actions/dataActions.js
@@ -97,6 +97,23 @@ export const postScream = (newScream) => (dispatch) => {
     })
 }
 
+export const getUserData = (userHandle) => dispatch => {
+  dispatch({ type: LOADING_DATA })
+  axios.get(baseUrl + `/user/${userHandle}`)
+    .then(res => {
+      dispatch({
+        type: SET_SCREAMS,
+        payload: res.data.screams
+      })
+    })
+    .catch(() => {
+      dispatch({
+        type: SET_SCREAMS,
+        payload: null
+      })
+    })
+}
+
 export const clearErrors = () => dispatch => {
   dispatch({ type: CLEAR_ERRORS })
 }
